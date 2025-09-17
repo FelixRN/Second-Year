e8ac94baf4e9802ea97a8809243e5b4111909ba4
FelixR <felixsr023@gmail.com>
2025-09-17 14:06:32 +0200

    PTM

--- JS/script.js
+++ JS/script.js
@@ -1,19 +1,23 @@
 function numeroPositivo(){
     let num = Number(prompt("Escribe un número: "));
     if(num > 0) {
-        return "El numero es positivo";
+        console.log("El numero es positivo");
+        return;
     }else if (num < 0){
-        return "El numero es negativo"
+        console.log("El numero es negativo");
+        return;
     } else{
-    return "El número es cero";
+        console.log("El número es cero");
+        return;
     }
 }
-numeroPositivo();
 
+numeroPositivo();
+/*
 function conversor(){
     let euro = Number(prompt("Escribe el valor en euros: "));
     let dolar = (euro * 8.44);
     return "Tienes " + dolar + " dolares";
 }
 
-conversor();
+conversor();*/
